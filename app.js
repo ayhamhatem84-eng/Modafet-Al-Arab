9a82cec1df4631904ce1f65d5ce23fe573ffeaaf
ayhamhatem84-eng <ayhamhatem84@gmail.com>
2026-04-27 12:48:47 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+let balance = 1000000000;
+let redDiamonds = 0;
+
+function sendGift(price) {
+    if (balance >= price) {
+        balance -= price;
+        redDiamonds += (price * 0.65); // الضيف يحصل على 65%
+        updateUI();
+        console.log("تم خصم الضريبة 35% لصالح السلطان أيهم");
+    } else {
+        alert("الرصيد غير كافٍ!");
+    }
+}
+
+function updateUI() {
+    document.getElementById('darahemBalance').innerText = balance.toLocaleString();
+    document.getElementById('diamondBalance').innerText = redDiamonds.toLocaleString();
+}
